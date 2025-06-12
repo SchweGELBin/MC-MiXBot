6d726793311de2ced1b630415b5e3e9b1950cbd3
SchweGELBin <abramjannikmichael06@gmail.com>
2025-06-12 21:04:45 +0200

    chore: add necessary files

---
+++ default.nix
@@ -0,0 +1,18 @@
+{ lib, rustPlatform }:
+
+rustPlatform.buildRustPackage (finalAttrs: {
+  pname = "mixbot";
+  version = "0.1.0";
+
+  src = ./.;
+  cargoLock.lockFile = ./Cargo.lock;
+
+  meta = {
+    description = "Minecraft MiXBot";
+    homepage = "https://github.com/SchweGELBin/MC-MiXBot";
+    changelog = "https://github.com/SchweGELBin/MC-MiXBot/blob/v${finalAttrs.version}/docs/CHANGELOG.md";
+    license = lib.licenses.mit;
+    mainProgram = finalAttrs.pname;
+    maintainers = with lib.maintainers; [ SchweGELBin ];
+  };
+})
---
+++ docs/README.md
@@ -0,0 +1,6 @@
+# MC-MiXBot
+[![GitHub Downloads (all assets, all releases)](https://img.shields.io/github/downloads/SchweGELBin/MC-MiXBot/total)](https://github.com/SchweGELBin/MC-MiXBot/releases)
+[![GitHub License](https://img.shields.io/github/license/SchweGELBin/MC-MiXBot)](../LICENSE)
+[![GitHub Release](https://img.shields.io/github/v/release/SchweGELBin/MC-MiXBot)](https://github.com/SchweGELBin/MC-MiXBot/releases/latest)
+
+Minecraft MiXBot
